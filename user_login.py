af6324e688825945cd8ff3cb0987e8239109f2a4
baiyuani <13835518617@163.com>
2020-03-18 18:46:56 +0800

    模拟用户登录

--- user_login.py
+++ user_login.py
@@ -1,23 +1,30 @@
 '''模拟用户登录'''
 
-import getpass
+from getpass import getpass as p
+import pickle
+import os
 
-ulist = {}
+def init():
+    if not os.path.exists('pass.txt'):
+        ulist = {'admin': 'password'}
+        with open('pass.txt', 'wb') as fn:
+            pickle.dump(ulist, fn)
 
 def logup():
     username = input('请输入用户名： ').strip()
     if username == '':
         print('用户名不能为空！')
-    elif username in ulist:
+    elif username in _list:
         print('用户已存在')
     else:
         password = input('请输入密码： ')
-        ulist[username] = password
+        _list[username] = password
 
 def login():
     username = input('请输入用户名： ')
-    password = getpass.getpass('请输入密码： ')
-    if ulist.get(username) == password:
+    password = p('请输入密码： ')
+    # password = input('mima: ')
+    if _list[username] == password:
         print('登录成功!')
     else:
         print('用户名或密码错误！')
@@ -33,9 +40,14 @@ def menu():
             print('输入错误！')
             continue
         if opt == '2':
+            with open('pass.txt', 'wb') as fn:
+                pickle.dump(_list, fn)
             print('bye')
             break
         cmds[opt]()
 
 if __name__ == '__main__':
+    init()
+    with open('pass.txt', 'rb') as fn:
+        _list = pickle.load(fn)
     menu()
