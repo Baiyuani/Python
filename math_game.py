29b90beb11d2ebafca1d4d70bae4896b2e0de72c
baiyuani <13835518617@163.com>
2020-03-20 15:07:33 +0800

    100以内四则算数: v2

--- math_game.py
+++ math_game.py
@@ -1,11 +1,14 @@
 import random
 
 def exam():
-    nums = [random.randint(1,100) for i in range(2)]
-    nums.sort(reverse=True)
-    opt = random.choice('+-×÷')
+    nums = [random.randint(1,100) for i in range(2)] #取两个随机数
+    nums.sort(reverse=True)  #排序
+    opt = random.choice('+-×÷')  #抽取选项
+    #函数字典
     dic = {'+': lambda x, y:x + y, '-': lambda x, y:x - y, '×': lambda x, y:x * y, '÷': lambda x, y:x // y}
+    #保存正确结果
     result = dic[opt](*nums)
+    #提示内容
     prompt = '%s %s %s = ' % (nums[0], opt, nums[1])
     count = 0
     while count < 3:
@@ -26,8 +29,12 @@ def exam():
 def menu():
     while 1:
         exam()
-        opt = input('继续吗？（y/n）')
-        if opt in 'Nn' and opt != '':
+        try:
+            #取出用户输入的两端空格，然后取出首字母
+            opt = input('继续吗？（y/n）').strip()[0]
+        except:
+            opt = 'y'
+        if opt in 'Nn':
             print('\nbye-bye')
             break
 
