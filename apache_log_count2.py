7f141d746ac88b978772ff025dacb1131f11d5ef
baiyuani <13835518617@163.com>
2020-03-23 10:34:21 +0800

    统计apache日志信息（OOP）:v2

--- apache_log_count2.py
+++ apache_log_count2.py
@@ -1,10 +1,13 @@
 import re
 
 class CountPatt:
-    def count_pattern(self, fname, patt):
+    def __init__(self, fname):
+        self.fname = fname
+
+    def count_pattern(self, patt):
         result = {}  #用于保存结果
         cpatt = re.compile(patt)
-        with open(fname) as fobj:
+        with open(self.fname) as fobj:
             for i in fobj:
                 m = cpatt.search(i)
                 if m:   #任何非空对象都为真
@@ -16,8 +19,11 @@ if __name__ == '__main__':
     logfile = 'access_log'
     ip = '^\d+(\.\d+){3}'
     br = 'Chrome|Firefox|MSIE'
-    example = CountPatt()
-    result1 = example.count_pattern(logfile, ip)
-    result2 = example.count_pattern(logfile, br)
+    oss = 'Windows|Linux'
+    example = CountPatt(logfile)
+    result1 = example.count_pattern(ip)
+    result2 = example.count_pattern(br)
+    result3 = example.count_pattern(oss)
     print(result1)
     print(result2)
+    print(result3)
