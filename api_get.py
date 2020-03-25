d52a9b73e8613b94702f6239024a202c451f0f78
baiyuani <13835518617@163.com>
2020-03-25 15:12:57 +0800

    api接口使用

---
+++ api_get.py
@@ -0,0 +1,8 @@
+import json
+import requests
+
+url = 'http://jisutqybmf.market.alicloudapi.com/weather/query'
+headers = {'Authorization': 'APPCODE 你的APPCODE'}
+params = {'citycode': '101010300'}
+r = requests.get(url, headers=headers, params=params)
+print(r.json())
